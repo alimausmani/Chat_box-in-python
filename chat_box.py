b58bf376679d0c05f15bd85f6703eec8acecce30
Alima Usmani <93898232+alimausman@users.noreply.github.com>
2022-05-02 15:13:39 +0530

    Add files via upload

---
+++ chat_box.py
@@ -0,0 +1,62 @@
+
+
+# Alima=(input("enter the letter:"))
+# if Alima=="Alima":
+#     print("hiii")
+#     Arisha=(input("enter the letter:"))
+#     if Arisha=="Arisha":
+#         print("hello")
+#         Alima=(input("enter the letter:"))
+#         if Alima=="Alima":
+#             print("how are you")
+#             Arisha=(input("enter the letter:"))
+#             if Arisha=="Arisha":
+#                 print("I am fine")
+#                 Arisha=(input("enter the letter"))
+#                 if Arisha=="Arisha":
+#                    print("and you")
+#                    Alima=(input("enter the letter"))
+#                    if Alima=="Alima":
+#                        print("I am good")
+#                        Arisha=(input("enter the letter"))
+#                        if Arisha=="Arisha":
+#                           print("what are you doing")
+#                           Alima=(input("enter the letter"))
+#                           if Alima=="Alima":
+#                                print("I am doing study and you")
+#                                Arisha=(input("enter the letter"))
+#                                if Arisha=="Arisha":
+#                                    print("I am doing home work")
+#                                    Alima=(input("enter the letter"))
+#                                    if Alima=="Alima":
+#                                        print("ok bye")
+#                                        Arisha=(input("enter the letter"))
+#                                        if Arisha=="Arisha":
+#                                           print("ok bye")
+#                                        else:
+#                                            print("not bye")
+#                                    else:
+#                                        print("what bye")
+#                                else:
+#                                    print("I am not doing home work")
+#                           else:
+#                                print ("I am not doing study")
+#                        else:
+#                            print("nothing")
+#                    else:
+#                        print("are you good")
+#                 else:
+#                     print("I am not good")
+#             else:
+#                 print("are you fine")
+#         else:
+#             print("are you not fine")
+#     else:
+#         print("what hiii")
+# else:
+#     print("nothing")
+
+
+
+
+
